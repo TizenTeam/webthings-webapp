17472d3ab8b699326904d2e0f3f95cdce8078020
Philippe Coval <p.coval@samsung.com>
2019-03-13 20:17:00 +0100

    WIP: mod: js/index.js (sandbox/rzr/devel/aframe/master)
    Change-Id: I5f6ecbfcfd531d3eac84f53dce7cdb0140a4d690
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- js/index.js
+++ js/index.js
@@ -391,7 +391,7 @@ ${endpoint}\
     });
 
     // PWA
-/*    if ('serviceWorker' in navigator) {
+    /*    if ('serviceWorker' in navigator) {
       try {
         navigator.serviceWorker.register('service-worker.js').then(
           function(registration) {
