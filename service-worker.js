8baf29f11be95cfda5a6ada14ccd8cd0ae3e76aa
Philippe Coval <p.coval@samsung.com>
2019-03-13 12:38:17 +0100

    WIP: mod: service-worker.js (sandbox/rzr/devel/aframe/master)
    Change-Id: Ia02632e58cb927e1c3c3f0adb9bbf8f0f569fbca
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- service-worker.js
+++ service-worker.js
@@ -22,26 +22,25 @@ var filesToCache = [
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+  e.waitUntil(caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
+      
+return cache.addAll(filesToCache);
+    }));
 });
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
+  e.waitUntil(caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
         if (key !== cacheName && key !== dataCacheName) {
           console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
+          
+return caches.delete(key);
         }
       }));
-    })
-  );
+    }));
+
   /*
    * Fixes a corner case in which the app wasn't returning the latest data.
    * You can reproduce the corner case by commenting out the line below and
@@ -57,14 +56,13 @@ self.addEventListener('activate', function(e) {
 
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
+
     /*
      * The app is asking for app shell files. In this scenario the app uses the
      * "Cache, falling back to the network" offline strategy:
      * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
      */
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
+    e.respondWith(caches.match(e.request).then(function(response) {
         return response || fetch(e.request);
-      })
-    );
+      }));
 });
