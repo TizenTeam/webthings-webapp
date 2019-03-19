430becfd0a5a884d5957cf821b72c712d92b8c1c
Philippe Coval <p.coval@samsung.com>
2019-03-19 14:45:49 +0100

    WIP: mod: js/index.js (sandbox/rzr/devel/aframe/master)
    Change-Id: Idf00c9faddea195f29fd01b7c7c7a22f69157231
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- js/index.js
+++ js/index.js
@@ -246,10 +246,9 @@
       if (!code && !isCallback) {
         return setTimeout(function() {
           const redirect_uri =
-                encodeURIComponent
-                (document.location.substring
-                (0,
-                 1 + document.location.lastIndexOf('/')));
+                encodeURIComponent(document.location
+                  .substring(0,
+                             1 + document.location.lastIndexOf('/')));
           const redirectUrl = `\
 ${localStorage.url}\
 ${authorize_endpoint}\
